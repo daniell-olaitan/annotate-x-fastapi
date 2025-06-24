c64eb81194690ffa6d82ca416f247957c3be989f
daniell-olaitan <daniell.olaitan@gmail.com>
2025-06-24 16:26:52 +0100

    Add db uri to env config

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -5,26 +7,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     secret_key: str
     database_username: str
-    database_host: str = 'localhost'
+    database_host: str
     database: str
     database_password: str
     cloudinary_cloud_name: str
     cloudinary_api_key: str
     cloudinary_api_secret: str
-    config: str = 'development'
-    database_uri: str = ''
+    config: str
+    database_uri: str
 
-    model_config = SettingsConfigDict(env_file='.env')
+    model_config = (
+        SettingsConfigDict(env_file='.env')
+        if os.getenv('CONFIG') == 'development'
+        else SettingsConfigDict()
+    )
 
 
 @lru_cache
 def get_settings() -> Settings:
     settings = Settings()
-    settings.database_uri = "postgresql://{}:{}@{}/{}".format(
-        settings.database_username,
-        settings.database_password,
-        settings.database_host,
-        settings.database
-    )
 
     return settings
